dc1b0e1e71780e21f999742d6803fa5f4a10bbcb
Anakorn Kyavatanakij <anakornk@gmail.com>
2021-12-09 16:10:58 +0700

    feat: add garden note page

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,10 +1,68 @@
 import { GatsbyNode } from 'gatsby';
+import path from 'path';
+import type { NoteTemplatePageContext } from './src/templates/NoteTemplate';
 
-export const createPages: GatsbyNode['createPages'] = async ({ actions }) => {
+type GatsbyNodeQuery = {
+  site: {
+    siteMetadata?: { gardenBasePath?: string };
+  };
+  allMarkdownRemark: {
+    edges: Array<{
+      node: {
+        id: string;
+        html: string;
+        parent?: { name?: string };
+      };
+    }>;
+  };
+};
+export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions;
-  createPage({
-    path: '/tpl',
-    component: require.resolve('./src/templates/tpl.tsx'),
-    context: {},
+
+  const result = await graphql<GatsbyNodeQuery>(
+    `
+      {
+        site {
+          siteMetadata {
+            gardenBasePath
+          }
+        }
+        allMarkdownRemark {
+          edges {
+            node {
+              id
+              html
+              parent {
+                ... on File {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  );
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  const gardenBasePath = result.data?.site?.siteMetadata?.gardenBasePath || '';
+  const posts = result.data?.allMarkdownRemark?.edges || [];
+
+  const noteTemplate = path.resolve('./src/templates/NoteTemplate.tsx');
+
+  posts.forEach(({ node }) => {
+    const { id, html } = node;
+    const fileName = node.parent?.name;
+
+    if (fileName) {
+      createPage<NoteTemplatePageContext>({
+        path: `${gardenBasePath}/${fileName}`,
+        component: noteTemplate,
+        context: { id, html },
+      });
+    }
   });
 };
---
+++ src/templates/NoteTemplate.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from '../components/layout';
+import { PageProps } from 'gatsby';
+import Seo from '../components/seo';
+
+export type NoteTemplatePageContext = {
+  id: string;
+  html: string;
+};
+
+export default function NoteTemplate({ pageContext }: PageProps<{}, NoteTemplatePageContext>) {
+  const { html } = pageContext;
+
+  return (
+    <Layout>
+      <Seo title="Note" />
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </Layout>
+  );
+}
