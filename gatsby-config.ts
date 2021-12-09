c86cf79877b66d428e0368daa77d25245effe3e2
Anakorn Kyavatanakij <anakornk@gmail.com>
2021-12-09 17:20:17 +0700

    feat: add community page and contributor list

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,9 +1,12 @@
+import contributors from './contributors.json';
+
 export const siteMetadata = {
   title: `Gatsby Default Starter`,
   description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
   author: `@gatsbyjs`,
   siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   gardenBasePath: '/garden',
+  contributors,
 };
 
 export const plugins = [
---
+++ src/pages/community.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { PageProps, Link, graphql } from 'gatsby';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
+
+const CommunityPage: React.FC<PageProps<GatsbyTypes.CommunityPageQuery>> = ({ data }) => {
+  const contributors = data.site?.siteMetadata?.contributors;
+
+  return (
+    <Layout>
+      <Seo title="Hashpire Community" />
+      <h1>Our Community</h1>
+      <div>
+        {contributors &&
+          contributors.map((contributor, index) => {
+            return <div key={index}>{contributor?.username || ''}</div>;
+          })}
+      </div>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  );
+};
+
+export default CommunityPage;
+
+export const query = graphql`
+  query CommunityPage {
+    site {
+      siteMetadata {
+        contributors {
+          displayName
+          email
+          github
+          shortIntro
+          username
+          website
+        }
+      }
+    }
+  }
+`;
