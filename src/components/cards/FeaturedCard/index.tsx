84993de23e4c7762b38cf4ba70cd8ccc28bad6c8
Parinya Rueangphun <parinya.rueangphun@gmail.com>
2021-12-17 15:02:20 +0700

    style: featured card

--- src/components/cards/FeaturedCard/index.tsx
+++ src/components/cards/FeaturedCard/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import classNames from 'classnames';
-import FolderSvg from '../../../icons/folder.inline.svg';
 import ContributorList from '../../ContributorList';
 import type { ContributorListProps } from '../../ContributorList';
 import { Link } from 'gatsby';
@@ -28,26 +27,19 @@ export default function FeaturedCard({
     <Link to={url}>
       <article
         className={classNames(
-          'h-80 max-w-md px-6 pb-6 flex flex-col justify-end rounded-2xl shadow-1 border border-neutral-400 bg-center bg-no-repeat bg-cover',
+          'h-80 max-w-md px-6 pb-6 flex flex-col justify-end rounded-2xl shadow-2 border border-brand-grey bg-center bg-no-repeat bg-cover',
           className,
         )}
         style={{
-          backgroundImage: `linear-gradient(180deg, rgba(30, 30, 30, 0.2) 0%, rgba(30, 30, 30, 0.1) 0.01%, rgba(30, 30, 30, 0.9) 72.97%, #1E1E1E 100%), url(${coverImage})`,
+          backgroundImage: `linear-gradient(180deg, rgba(23, 23, 23, 0) 0.01%, rgba(23, 23, 23, 0.6) 43.23%, rgba(23, 23, 23, 0.9) 71.87%, #171717 100%),url(${coverImage}`,
         }}
       >
-        <div className="flex flex-col justify-between">
-          <div className="flex flex-col">
-            <div className="flex items-center">
-              <FolderSvg className="w-8 h-8 text-neutral-100" />
-              <span className="ml-2 font-semibold text-neutral-100 line-clamp-1">{title}</span>
-            </div>
-            <span className="mt-1 text-sm font-normal line-clamp-2 text-neutral-500">{excerpt}</span>
-          </div>
-          <div className="flex items-center mt-3">
-            <span className="text-xs font-normal text-neutral-500">{publishedAt}</span>
-            <div className="w-0.5 h-0.5 rounded-full bg-neutral-400 mx-2" />
-            <ContributorList contributors={contributors} />
-          </div>
+        <span className="block text-xl font-semibold text-neutral-100 line-clamp-2">{title}</span>
+        <p className="mt-1 text-sm font-normal line-clamp-2 text-neutral-400">{excerpt}</p>
+        <div className="flex items-center mt-4">
+          <span className="text-xs font-normal text-neutral-400">{publishedAt}</span>
+          {contributors.length >= 1 && <div className="w-1 h-1 mx-2 rounded-full bg-neutral-400" />}
+          <ContributorList contributors={contributors} />
         </div>
       </article>
     </Link>
