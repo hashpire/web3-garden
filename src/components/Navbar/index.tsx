a8f41b19913404c988146d762f79e7fe51da46c7
Parinya Rueangphun <parinya.rueangphun@gmail.com>
2021-12-21 13:46:23 +0700

    feat: navbar active style

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -14,20 +14,20 @@ const Navbar: React.FC<NavbarProps> = () => {
   return (
     <nav className="flex flex-grow h-12 lg:h-16">
       <ul className="flex justify-around w-full h-full max-w-lg m-auto lg:max-w-full lg:justify-start">
-        <li className="lg:ml-10">
-          <Link to="/" className="flex items-center h-full text-gray-400 hover:text-primary">
+        <li className="text-gray-400 hover:text-primary lg:ml-10">
+          <Link to="/" className="flex items-center h-full" activeClassName="text-primary">
             <HomeSvg className="w-7 h-7 lg:w-6 lg:h-6" />
             <span className="hidden ml-2 text-sm font-semibold md:inline-block ">Home</span>
           </Link>
         </li>
-        <li className="lg:ml-10">
-          <Link to="/community" className="flex items-center h-full text-gray-400 hover:text-primary">
+        <li className="text-gray-400 hover:text-primary lg:ml-10">
+          <Link to="/community" className="flex items-center h-full" activeClassName="text-primary">
             <PeopleSvg className="w-7 h-7 lg:w-6 lg:h-6" />
             <span className="hidden ml-2 text-sm font-semibold md:inline-block ">Community</span>
           </Link>
         </li>
-        <li className="lg:ml-10">
-          <Link to="/about" className="flex items-center h-full text-gray-400 hover:text-primary">
+        <li className="text-gray-400 hover:text-primary lg:ml-10">
+          <Link to="/about" className="flex items-center h-full" activeClassName="text-primary">
             <BulbSvg className="w-7 h-7 lg:w-6 lg:h-6" />
             <span className="hidden ml-2 text-sm font-semibold md:inline-block ">About</span>
           </Link>
