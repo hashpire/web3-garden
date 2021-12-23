79f1c3b934381694162a8b09d89e27ea3d844ad6
Parinya Rueangphun <parinya.rueangphun@gmail.com>
2021-12-23 15:57:35 +0700

    feat: add toggle collapse pane

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,24 +2,37 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Navbar from './Navbar';
 import HashpireLogoSvg from '@images/hashpire-logo-dark.inline.svg';
-// import LeftPaneSvg from '@icons/left-pane.inline.svg';
-// import RightPaneSvg from '@icons/right-pane.inline.svg';
+import LeftPaneSvg from '@icons/left-pane.inline.svg';
+import RightPaneSvg from '@icons/right-pane.inline.svg';
 
 type HeaderProps = {
+  sidePane?: {
+    onLeftClick: () => void;
+    onRightClick: () => void;
+  };
   siteTitle: string;
 };
 
-const Header: React.FC<HeaderProps> = ({ siteTitle }) => {
-  // px-4 sm:px-6 md:px-8
+const Header: React.FC<HeaderProps> = ({ siteTitle, sidePane }) => {
   return (
     <header className="flex flex-col px-2 py-2 border-b border-solid lg:px-6 lg:py-0 bg-background-darker border-background shadow-2 lg:shadow-none lg:bg-background lg:flex-row">
       <div className="flex items-center h-16">
-        {/* <LeftPaneSvg className="block lg:hidden text-neutral-500 w-9 h-9" /> */}
+        {sidePane?.onLeftClick && (
+          <LeftPaneSvg
+            className="block lg:hidden text-neutral-500 w-9 h-9 hover:text-primary"
+            onClick={sidePane.onLeftClick}
+          />
+        )}
         <Link to="/" className="flex h-full mx-auto">
           <span className="sr-only">{siteTitle}</span>
           <HashpireLogoSvg className="w-32 h-auto" />
         </Link>
-        {/* <RightPaneSvg className="block lg:hidden text-neutral-500 w-9 h-9" /> */}
+        {sidePane?.onRightClick && (
+          <RightPaneSvg
+            className="block lg:hidden text-neutral-500 w-9 h-9 hover:text-primary"
+            onClick={sidePane.onRightClick}
+          />
+        )}
       </div>
       <Navbar />
     </header>
--- src/components/layouts/NoteLayout/index.tsx
+++ src/components/layouts/NoteLayout/index.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../../Header';
 import DesktopPane from './DesktopPane';
-import { useLg } from '../../../hooks/responsive';
+import CollapsePane from '../../CollapsePane';
+import { useLg, useXl } from '../../../hooks/responsive';
 import MobilePane from './MobilePane';
 
 export type NoteLayoutProps = {};
 
 const NoteLayout: React.FC<NoteLayoutProps> = ({ children }) => {
   const isLg = useLg();
+  const isXl = useXl();
   const [mobilePane, setMobilePane] = useState<'left' | 'right' | null>(null);
-  const [leftPaneVisible, setLeftPaneVisbile] = useState<boolean>(true);
-  const [rightPaneVisible, setRightPaneVisible] = useState<boolean>(true);
+  const [leftPaneVisible, setLeftPaneVisbile] = useState<boolean>(isXl ? true : false);
+  const [rightPaneVisible, setRightPaneVisible] = useState<boolean>(isXl ? true : false);
 
   // lock and unlock body scroll when menu overlay visible
   useEffect(() => {
@@ -22,31 +24,54 @@ const NoteLayout: React.FC<NoteLayoutProps> = ({ children }) => {
   }, [mobilePane]);
 
   return (
-    <div className="flex flex-col min-h-screen bg-background lg:h-screen">
-      <Header siteTitle={`Garden`} />
+    <div className="flex flex-col min-h-screen lg:h-screen">
+      <Header
+        siteTitle={`Garden`}
+        sidePane={{
+          onLeftClick: () => setMobilePane((value) => (value ? null : 'left')),
+          onRightClick: () => setMobilePane((value) => (value ? null : 'right')),
+        }}
+      />
       {/* overflow-x-hidden to remove scrollbar during animation */}
-      <div className="flex flex-row flex-1 overflow-x-hidden border-t lg:overflow-y-hidden border-brand-grey">
+      <div className="flex flex-row flex-1 overflow-x-hidden lg:overflow-y-hidden">
         {/* All items in row will take the largest height of them by default. */}
         {!isLg && (
           <MobilePane isShowing={mobilePane === 'left'} direction="right" onClose={() => setMobilePane(null)}>
-            <div>Left Pane</div>
+            <span className="text-white">Left Pane</span>
           </MobilePane>
         )}
-        {isLg && <DesktopPane isShowing={leftPaneVisible}>test</DesktopPane>}
-        {/* https://www.w3.org/TR/css-flexbox-1/#min-size-auto */}
-        <main className="flex flex-col flex-1 min-w-0 lg:border-l lg:border-r lg:overflow-y-auto bg-background-darker">
-          {isLg && <button onClick={() => setLeftPaneVisbile((isShowing) => !isShowing)}>Left</button>}
-          {isLg && <button onClick={() => setRightPaneVisible((isShowing) => !isShowing)}>Right</button>}
-          {!isLg && <button onClick={() => setMobilePane((value) => (value ? null : 'left'))}>Mobile Left</button>}
-          {!isLg && <button onClick={() => setMobilePane((value) => (value ? null : 'right'))}>Mobile Right</button>}
-          {children}
+        {isLg && (
+          <DesktopPane isShowing={leftPaneVisible}>
+            <span className="text-white">Left Pane</span>
+          </DesktopPane>
+        )}
+        {isLg && (
+          <CollapsePane
+            direction={leftPaneVisible ? 'left' : 'right'}
+            className="border-r border-brand-grey"
+            onClick={() => setLeftPaneVisbile((isShowing) => !isShowing)}
+          />
+        )}
+        <main className="flex flex-col flex-1 min-w-0 bg-neutral-900 lg:overflow-y-auto">
+          <div className="max-w-4xl mx-auto">{children}</div>
         </main>
+        {isLg && (
+          <CollapsePane
+            direction={rightPaneVisible ? 'right' : 'left'}
+            className="border-l border-brand-grey"
+            onClick={() => setRightPaneVisible((isShowing) => !isShowing)}
+          />
+        )}
         {!isLg && (
           <MobilePane isShowing={mobilePane === 'right'} direction="left" onClose={() => setMobilePane(null)}>
-            <div className="bg-blue-500">Right Pane</div>
+            <span className="text-white">Right Pane</span>
           </MobilePane>
         )}
-        {isLg && <DesktopPane isShowing={rightPaneVisible}>test</DesktopPane>}
+        {isLg && (
+          <DesktopPane isShowing={rightPaneVisible}>
+            <span className="text-white">Right Pane</span>
+          </DesktopPane>
+        )}
       </div>
     </div>
   );
