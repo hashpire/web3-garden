dac26fccb3dc1708b052062147d29698ada150d3
Anakorn Kyavatanakij <anakornk@gmail.com>
2022-01-04 00:51:15 +0700

    fix: fix 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,39 +1,36 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { PageProps } from 'gatsby';
-import Seo from '../components/Seo';
-import NotfoundSvg from '@images/not-found.inline.svg';
+import Seo from '@/components/Seo';
+import NotFoundSvg from '@images/not-found.inline.svg';
 import backgroundDot from '@images/background-dot.png';
 import WaveSvg from '@images/wave.inline.svg';
-import Header from '../components/Header';
 
 const NotFoundPage: React.FC<PageProps> = () => (
-  <div
-    className="flex flex-col min-h-screen bg-no-repeat bg-cover"
+  <main
+    className="flex flex-col items-center flex-1"
     style={{
       backgroundImage: `url(${backgroundDot})`,
     }}
   >
-    <Header />
-    <Seo title="404: Not found" />
-    <main className="flex flex-col justify-center flex-grow px-6 text-center md:py-16">
-      <NotfoundSvg className="mx-auto" />
-      <span className="block text-3xl font-semibold mt-9 text-primary md:text-4xl md:mt-12">
-        Page not found
-      </span>
-      <p className="mt-4 font-semibold whitespace-pre-line tex-base text-neutral-200 md:mt-6 lg:whitespace-normal">
-        {`Looks like you’re lost. \nYou just hit a tree that doesn’t exist in the garden`}
-      </p>
-      <Link to="/">
-        <button className="px-8 py-3 mt-12 text-base font-semibold border text-primary rounded-3xl border-primary hover:text-neutral-200 hover:bg-primary lg:mt-16">
-          Back to home
-        </button>
-      </Link>
-    </main>
-    <div className="w-full overflow-hidden max-h-80">
-      <WaveSvg className="w-full h-full" />
+    <Seo title="Page Not Found" />
+    <NotFoundSvg />
+    <h1 className="text-3xl font-semibold mt-9 text-primary md:text-4xl md:mt-12">
+      Page not found
+    </h1>
+    <p className="mt-4 text-base font-semibold whitespace-pre-line text-neutral-200 md:mt-6 lg:whitespace-normal">
+      {`Looks like you’re lost. \nYou just hit a tree that doesn’t exist in the garden`}
+    </p>
+    <Link
+      to="/"
+      className="px-8 py-3 mt-12 text-base font-semibold border text-primary rounded-3xl border-primary hover:text-neutral-200 hover:bg-primary lg:mt-16"
+    >
+      Back to Home
+    </Link>
+    <div className="w-full mt-auto overflow-hidden max-h-80">
+      <WaveSvg className="w-full h-auto" />
     </div>
-  </div>
+  </main>
 );
 
 export default NotFoundPage;
