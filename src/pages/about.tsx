cd454778ed8bda9266441bdee8dcd25392dde3b1
Anakorn Kyavatanakij <anakornk@gmail.com>
2022-01-04 20:03:53 +0700

    chore: fix about page wordings

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,33 +8,37 @@ import backgroundDot from '../images/background-dot.png';
 import Seo from '@/components/Seo';
 import { Link } from 'gatsby';
 
-const whyjoin = [
+const communityGoals = [
+  `Provide a comprehensive \nWeb3 Digital Garden \n(Knowledge Graph)`,
+  `A friendly space for Web3 enthusiasts to network and discuss all things Web3`,
+  `Promote and support the growth and development of the decentralized web`,
+];
+
+const communityPerks = [
   `Meet friendly \nlike-minded people`,
-  `Learning everything \nabout web3`,
-  `Participate in events \n(e.g. hackathons, CTF)`,
-  `Showcase what \nyou are working on`,
-  `Get monthly prizes \nfor content published`,
-  `Recieve apportunity \nfrom partners and sponsors`,
+  `Knowledge sharing and \ncollaborative learning with peers`,
+  `Participate in community\n events and activities`,
+  `Recognition and rewards \nfor your contributions`,
+  `Access to exclusive privileges\n from sponsors and partners`,
+  `Have a voice in our community \nand so much more!`,
+  // `Mentorships, Sponsorships, Investments and Job Opportunities`,
 ];
 
 const digitalGarden = [
   {
     icon: <SeedSvg className="text-neutral-200 w-14 h-14" />,
-    title: 'Seed',
-    description: 'Seed the garden with a \ncontent idea.',
-    subDescription: 'Start a topic with an idea',
+    title: 'Plant Seeds',
+    description: 'Create new content ideas',
   },
   {
     icon: <TreeSvg className="text-neutral-200 w-14 h-14" />,
-    title: 'Tree',
-    description: `Produce branches \nfor the content.`,
-    subDescription: 'Create content linked to the topic',
+    title: 'Nourish Trees',
+    description: 'Create and link related content',
   },
   {
     icon: <GardenSvg className="text-neutral-200 w-14 h-14" />,
-    title: 'Garden',
-    description: `Connect the dots \n\n`,
-    subDescription: 'See relationships and gain insights',
+    title: 'Walk in the Garden',
+    description: 'See content relationships and gain insights',
   },
 ];
 
@@ -48,14 +52,34 @@ const AboutPage: React.FC<{}> = () => (
         backgroundImage: `url(${backgroundDot})`,
       }}
     >
-      <div className="mx-auto text-center max-w-7xl lg:whitespace-pre-line">
-        <span className="text-3xl font-semibold text-primary md:text-4xl">
-          hashpire
-        </span>
-        <p className="inline text-3xl font-semibold whitespace-pre-line text-neutral-100 md:text-4xl xl:whitespace-normal">{` is \na community-driven \nWeb3 Digital Garden`}</p>
-        <p className="mt-10 text-base font-semibold text-neutral-400 md:mt-6 md:text-xl md:whitespace-pre-line">
-          {`A space for people to create, curate, cultivate and discuss content \nrelated to the Decentralize web`}
+      <div className="mx-auto text-left md:text-center max-w-7xl lg:whitespace-pre-line">
+        <h1 className="text-4xl font-semibold whitespace-pre-line md:mt-6 md:text-6xl text-neutral-100 md:whitespace-normal">
+          {`In\n Decentralization\n We Trust`}
+        </h1>
+        <p className="mt-10 text-3xl font-semibold whitespace-pre-line text-neutral-100 md:text-4xl xl:whitespace-normal">
+          <span className="md:text-4xl text-primary">hashpire</span>
+          {
+            '\n is a collaborative space for people to learn, share and discuss all things Web3'
+          }
         </p>
+      </div>
+    </section>
+
+    <section className="px-6 py-16 bg-background-darker md:py-20 md:px-8 lg:py-28 lg:px-16">
+      <div className="mx-auto text-center max-w-7xl">
+        <h2 className="text-3xl font-semibold text-primary md:text-4xl">
+          Our Goals
+        </h2>
+        <div className="grid grid-cols-1 gap-12 mt-12 md:grid-cols-3 md:gap-6 lg:mt-24 lg:gap-20">
+          {communityGoals.map((goal, key) => (
+            <div className="text-center whitespace-pre-line" key={key}>
+              <span className="inline-block w-4 h-4 rounded-full bg-primary" />
+              <span className="block mt-2 text-base font-semibold text-neutral-200 lg:text-xl">
+                {goal}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
 
@@ -64,10 +88,10 @@ const AboutPage: React.FC<{}> = () => (
       <div className="mx-auto text-center max-w-7xl xl:grid xl:grid-rows-2 xl:grid-flow-col gap-x-12">
         <div className="xl:mt-8">
           <h2 className="text-3xl font-semibold text-primary md:text-4xl">
-            Digital Garden
+            Building a Web3 Digital Garden
           </h2>
           <p className="mt-6 text-base font-semibold text-neutral-400 md:text-xl lg:whitespace-pre-line xl:whitespace-normal">
-            {`A digital garden is a public space of information where seeds of thought grow. \nIt's about writing and creating process-oriented networking content.`}
+            {`In a rapidly changing technological world, our community is committed to building and maintaining a community-driven Digital Garden, a graph-based collective knowledge base, to provide valuable knowledge and powerful insights about Web3 to the general public.`}
           </p>
         </div>
         <IdeaPotSvg className="inline mt-12 xl:block xl:mt-0 xl:w-full xl:h-full xl:order-first xl:row-span-2 xl:col-span-2" />
@@ -84,9 +108,6 @@ const AboutPage: React.FC<{}> = () => (
               <span className="block mt-2 text-base font-semibold md:mt-3 text-neutral-200">
                 {element.description}
               </span>
-              <span className="block mt-2 text-base font-normal md:mt-3 text-neutral-400">
-                {element.subDescription}
-              </span>
             </div>
           ))}
         </div>
@@ -97,14 +118,14 @@ const AboutPage: React.FC<{}> = () => (
     <section className="px-6 py-16 bg-background-darker md:py-20 md:px-8 lg:py-28 lg:px-16">
       <div className="mx-auto text-center max-w-7xl">
         <h2 className="text-3xl font-semibold text-primary md:text-4xl">
-          Why join ?
+          Community Perks
         </h2>
         <div className="grid grid-cols-1 gap-12 mt-12 md:grid-cols-3 md:gap-6 lg:mt-24 lg:gap-20">
-          {whyjoin.map((post, key) => (
+          {communityPerks.map((perk, key) => (
             <div className="text-center whitespace-pre-line" key={key}>
               <span className="inline-block w-4 h-4 rounded-full bg-primary" />
               <span className="block mt-2 text-base font-semibold text-neutral-200 lg:text-xl">
-                {post}
+                {perk}
               </span>
             </div>
           ))}
@@ -113,7 +134,7 @@ const AboutPage: React.FC<{}> = () => (
           to="/community"
           className="inline-block px-8 py-3 mt-12 text-base font-semibold border text-primary rounded-3xl border-primary hover:text-neutral-200 hover:bg-primary lg:mt-24"
         >
-          Join Our Community
+          Join Us
         </Link>
       </div>
     </section>
